0eb478bf99fa8faa237e59a1268926e2005cf40b
Ashish Ulhe <145021238+Ashishulhe@users.noreply.github.com>
2026-05-23 12:04:56 +0530

    Time: 2 ms (99.35%), Space: 46.6 MB (66.17%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,4 +1,46 @@
-class Solution {
+class Solution 
+{
+    public String reverseVowels(String s)
+    {
+        //fisrt create boolean array
+        boolean isVowel[]=new boolean[128];
+        //now for vowels set true;
+        for(char c:"aeiouAEIOU".toCharArray())
+        {
+            isVowel[c]=true;
+        }
+        
+        //now convert the string to charArray
+        char arr[]=s.toCharArray();
+        int start=0;
+        int end=arr.length-1;
+        while(start<end)
+        {
+            boolean isStartVowel=isVowel[arr[start]];
+            boolean isEndVowel=isVowel[arr[end]];
+            if(isStartVowel && isEndVowel) //both are vowel swap
+            {
+                char temp=arr[start];
+                arr[start]=arr[end];
+                arr[end]=temp;
+                start++;
+                end--;
+            }
+            else if(!isStartVowel)
+            {
+                start++;
+            }
+            else if(!isEndVowel)
+            {
+                end--;
+            }
+            
+        }
+        return new String(arr);
+    }
+}
+
+/*class Solution {
     public String reverseVowels(String s) {
         int start=0;
         int end=s.length()-1;
@@ -25,29 +67,30 @@ class Solution {
                 end--;
             }
 
-            /*if  (  (String.valueOf(startChar).matches("^[AEIOUaeiou]$")) &&
-                    (String.valueOf(endChar).matches("^[AEIOUaeiou]$"))  )
-                    {
-                        sb.setCharAt(start,endChar);
-                        sb.setCharAt(end,startChar);
-                        start++;
-                        end--;
-                    }
-            else if( ! (String.valueOf(startChar).matches("^[AEIOUaeiou]$")))
-            {
-                start++;
-            }
-            else if( !String.valueOf(endChar).matches("^[AEIOUaeiou]$")) 
-            {
-                end--;
-            }
-            */
+            // if  (  (String.valueOf(startChar).matches("^[AEIOUaeiou]$")) &&
+            //         (String.valueOf(endChar).matches("^[AEIOUaeiou]$"))  )
+            //         {
+            //             sb.setCharAt(start,endChar);
+            //             sb.setCharAt(end,startChar);
+            //             start++;
+            //             end--;
+            //         }
+            // else if( ! (String.valueOf(startChar).matches("^[AEIOUaeiou]$")))
+            // {
+            //     start++;
+            // }
+            // else if( !String.valueOf(endChar).matches("^[AEIOUaeiou]$")) 
+            // {
+            //     end--;
+            // }
+            
             
         }
         return sb.toString();
         
     }
 }
+*/
 
 /*
 if ((startChar=='A'||startChar=='E'||startChar=='I'||startChar=='O'||startChar='U'||
