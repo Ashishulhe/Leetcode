59244be988d488f9e22fea03270333f1abc73d0a
Ashish Ulhe <145021238+Ashishulhe@users.noreply.github.com>
2026-06-02 10:23:18 +0530

    Time: 2 ms (18.6%), Space: 42.8 MB (97.43%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,5 +1,41 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+
+        int i=0;// for traversing word1
+        int j=0; //for traversing word2
+        int n=0; // for accessing character of word1 any index
+        int m=0;  // for accessing character of word2 any index
+        while(i<word1.length && j<word2.length)
+        {
+            //compare character 
+            if(word1[i].charAt(n)  !=word2[j].charAt(m)) //if not equal return false
+            {
+                return false;
+            }
+            n++;
+            m++;
+            if(n==word1[i].length())
+            {
+                i++; //increse i cause the String eneded next string  now
+                n=0;  // reset index cause String index start from 0
+                
+            }
+            if(m==word2[j].length())
+            {
+                j++; //increse i cause the String eneded next string  now
+                m=0;  // reset index cause String index start from 0   
+            }
+        }
+        if( (i==word1.length && j<word2.length) || (j==word2.length && i<word1.length) )
+        {
+            return false;
+        }
+        return true;
+    }
+}
+/*
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         //create two stringBuilder 
         StringBuilder sb1= new StringBuilder();
         StringBuilder sb2= new StringBuilder();
@@ -14,3 +50,5 @@ class Solution {
         return sb1.toString().equals(sb2.toString());
     }
 }
+
+ */
