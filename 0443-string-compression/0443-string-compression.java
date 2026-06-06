8bdf98ba6e1e8affc768a239a86453f2f709bddc
Ashish Ulhe <145021238+Ashishulhe@users.noreply.github.com>
2026-06-06 09:59:11 +0530

    Time: 1 ms (99.92%), Space: 45.2 MB (78.72%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,4 +1,60 @@
-class Solution {
+
+class Solution 
+{
+    public int compress(char[] chars) 
+    {
+        if(chars.length==1)
+         return 1;
+        
+        int count=1;
+        int j=0;// for storing values in chars[] modifying array
+        int i=0;
+        for(i=1;i<chars.length;i++)
+        {
+            if(chars[i-1]==chars[i])
+            {
+                count++;
+            }
+            else
+            {
+               chars[j++]=chars[i-1];
+               if(count>1)
+               {
+                    String str=String.valueOf(count);
+                    
+                    for(int m=0;m<str.length();m++)
+                        {
+                            
+                            
+                                chars[j++]=str.charAt(m);
+                        
+                        }
+               }
+                            
+                    
+               count=1;
+                
+            }
+        }
+        chars[j++]=chars[i-1];
+        if(count>1)
+        {
+           String str=String.valueOf(count);
+                    
+            for(int m=0;m<str.length();m++)
+            {            
+               chars[j++]=str.charAt(m);
+                        
+            }
+        }
+        return j;
+
+    }
+}
+
+
+
+/*class Solution {
     public int compress(char[] chars) {
         if(chars.length==1)
          return 1;
@@ -31,4 +87,4 @@ class Solution {
 
         
     }
-}
+}*/
