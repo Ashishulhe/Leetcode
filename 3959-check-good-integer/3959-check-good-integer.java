02668707b340fa778459ed2973c3e1cee38e9ecc
Ashish Ulhe <145021238+Ashishulhe@users.noreply.github.com>
2026-06-29 17:02:06 +0530

    Time: 1 ms (95.52%), Space: 42.5 MB (66.78%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int sum=0;
+        int squareSum=0;
+
+        while(n!=0)
+        {
+            int digit=n%10;
+            sum=sum+digit;
+            squareSum=squareSum+ digit*digit;
+            n=n/10;
+        }
+       return (squareSum-sum)>=50; 
+    }
+}
