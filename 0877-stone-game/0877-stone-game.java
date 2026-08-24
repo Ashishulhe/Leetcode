d43409dce28cbec3f123d8ccd2121ccad587c271
Ashish Ulhe <145021238+Ashishulhe@users.noreply.github.com>
2026-08-24 15:48:40 +0530

    Time: 8 ms (25.55%), Space: 46.6 MB (25.37%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,32 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n=piles.length;
+
+        //create dp array
+        int dp[][]= new int[n][n];
+        //base case
+        //if one one length then slice will take it
+        for(int i=0;i<n;i++)
+        {
+            dp[i][i]=piles[i];
+        }
+
+        //now for diff lenth range
+        for(int len=2;len<=n;len++)
+        {
+            for(int i=0;i+len<=n ; i++)
+            {
+                int j=i+len-1;
+
+                //take left
+                int takeleft= piles[i]-dp[i+1][j];
+
+                //if take right
+                int takeright=piles[j]-dp[i][j-1];
+
+                dp[i][j]=Math.max(takeleft, takeright);
+            }
+        }
+        return dp[0][n-1]>0 ;
     }
 }
